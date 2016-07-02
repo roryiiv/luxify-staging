d7da047722a1a9c336d0920e47813ab59c23dec2
Martins <max.lui@gmail.com>
2016-07-02 10:57:49 +0800

    Add 3D VR tour into the slider

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -7,15 +7,58 @@
     <link rel="stylesheet" href="/assets/css/main2.css">
 @endsection
 @section('content')
+    <style>
+     .Tour3DCTA {
+       color: white;
+       font-size: 46px;
+       text-align: center;
+       width: 214px;
+       position: relative;
+       top: 53px;
+       left: 188px;
+       text-transform: UPPERCASE;
+       font-family: 'Roboto';
+       font-weight: 100;
+     } 
+    </style>
     <!-- main banner of the page -->
     <div class="inner-banner">
         <!-- banner image -->
         <section class="images">
             <?php $images = json_decode($listing->images); //var_dump($images); exit; ?>
             <ul>
+                @if($listing->aerialLook3DUrl)
+                  <!--<li>
+                    <a rel="lightbox_3D" class="3DTour fancybox fancybox.iframe" href="{{$listing->aerialLook3DUrl}}">
+
+                      <div style="min-height: 397px; min-width: 600px; background-color: rgba(0,0,0,0.6); position: absolute;">
+                         <h2 class='Tour3DCTA'>3D Virtual Tour Available
+                         </h2>
+                      </div>
+                        <div style="min-height: 397px; min-width: 600px; background-image:url('/assets/images/3DTour_sample_2.gif'); background-repeat: no-repeat; background-size: cover;"></div>  
+                    </a>
+                  </li>-->
+                  <li>
+                    <a style="position: relative;" rel="lightbox_3D" class="3DTour fancybox fancybox.iframe" href="{{$listing->aerialLook3DUrl}}">
+                      <div style="width:624px; height: 33rem; position: absolute;" >
+                         <h2 class='Tour3DCTA' style="z-index: 2;">3D Virtual Tour
+                           <br />
+                           <span style="margin-top: 20px; z-index:2" class="glyphicon glyphicon-play-circle"></span>
+                         </h2>
+                         <div style="width: 100%;height: 100%;background-color:rgba(0,0,0, 0.5);position: absolute; z-index: 1;top: 0;"></div>
+                      </div>
+                      <img style="z-index:1;"src="/assets/images/3DTour_sample_2.gif">
+                      
+                    </a>
+                  </li>
+                @endif
                 @if(is_array($images))
                     @foreach($images as $image)
-                        <li><img src="{{ func::img_url($image, 980) }}" /></li>
+                        <li>
+                          <a rel="lightbox" href="{{func::img_url($image, 1024)}}" class="fancybox">
+                            <img src="{{ func::img_url($image, 600) }}" />
+                          </a>
+                        </li>
                     @endforeach
                 @else
                     <li><img src="{{ func::img_url($listing->mainImageUrl, 980) }}" /></li>
@@ -340,15 +383,29 @@
             });
         });
         $(document).ready(function() {
+
+    		$(".3DTour").fancybox({
+        	fitToView	: true,
+        	width		: '90%',
+        	height		: '90%',
+        	autoSize	: true,
+        	closeClick	: false,
+        	openEffect	: 'none',
+        	closeEffect	: 'none',
+          arrows: false,
+          mouseWheel: false,
+        });
+
     		$(".lightbox").fancybox({
-        		fitToView	: false,
-        		width		: '70%',
-        		height		: '70%',
-        		autoSize	: false,
-        		closeClick	: false,
-        		openEffect	: 'none',
-        		closeEffect	: 'none'
-            });
+        	fitToView	: false,
+        	width		: '80%',
+        	height		: '80%',
+        	autoSize	: false,
+        	closeClick	: false,
+        	openEffect	: 'none',
+        	closeEffect	: 'none'
+        });
+
     	});
     });
     </script>
