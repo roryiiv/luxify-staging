d6ed4e7dea4b95d021a7afad0069389084db2d7b
Martins <max.lui@gmail.com>
2016-07-09 17:46:41 +0800

    Fixed for press link

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -99,7 +99,7 @@
                                             <ul>
                                                 <li><a href="/about">About us / Careers</a></li>
                                                 <li><a href="/why-luxify">How it works</a></li>
-                                                <li><a href="#">Press</a></li>
+                                                <li><a target="_blank" href="http://press.luxify.com">Press</a></li>
 												<li><a href="http://blog.luxify.com/">Blog</a></li>
                                             </ul>
                                         </nav>
