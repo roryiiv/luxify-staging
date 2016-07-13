22d71cdae2849470e5e531a246f2d2d07c00583c
Ubuntu <ubuntu@ip-172-31-28-49.ap-southeast-1.compute.internal>
2016-07-13 01:00:41 +0000

    Hotfixs after deployment

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,9 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+
+        // appending custom middleware
+        'App\Http\Middleware\HttpsRouting',
     ];
 
     /**
@@ -29,6 +32,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+            \App\Http\Middleware\HttpsRouting::class,
         ],
 
         'api' => [
--- app/Http/Middleware/HttpsRouting.php
+++ app/Http/Middleware/HttpsRouting.php
@@ -3,22 +3,20 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated
+class HttpsRouting
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/');
+        if (!$request->secure() && env('APP_ENV') === 'production') {
+            // return redirect()->secure($request->getRequestUri());
         }
 
         return $next($request);
