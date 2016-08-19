155aab631307d96efa1c7da3b000413e86ac1120
Martins <max.lui@gmail.com>
2016-08-19 16:20:06 +0800

    Change the translations

---
+++ resources/lang/en_us/footer.php
@@ -0,0 +1,27 @@
+<?php
+
+//en_us - frontend - footer
+//for file: layout.front.blade.php
+
+return[
+    
+    'aboutLuxify' => 'Luxify',
+    'aboutUsCareers'=>'About us / Careers',
+    'howItWorks'=>'How it works',
+    'press'=>'Press',
+    'blog' => 'Blog',
+    'dealerDirectory'=>'Dealer Directory',
+	  'platform'=>'Platform',
+    'logout'=>'Logout',
+    'memberLogin'=>'Member Login',
+    'pricing'=>'Pricing',
+    'dealerApplication'=>'Dealer Application',
+    'luxifyEstates'=>'Luxify Estates',
+    'contactUs'=>'Contact Us',
+    'ourOffice'=>'Our Office',
+    'followUs'=>'Follow us',
+    'terms' => 'Terms of Service',
+    'policy' => 'Privacy Policy',
+    'companyName' => 'Luxify Limited.',
+    'rightReserved' => 'All Rights Reserved',
+];
---
+++ resources/lang/zh_hk/home.php
@@ -0,0 +1,16 @@
+<?php
+// zn_cn
+return[
+//file:layouts.front
+
+//file:lisitng
+    'listing_dealerPage'=>'商家頁面',
+    'listing_contactDealer'=>'聯系商家',
+    'listing_dealerSince'=>'自',
+    'listing_mftseller'=>'更多來自此商家的商品',
+    'listing_ymalike'=>'你也許會喜歡的商品',
+//file:dealer
+    'dealer_mftseller'=>'更多來自此商家的商品',
+    'dealer_viewlistings'=>'查看所有商品',
+    'dealer_contactDealer'=>'聯系商家',
+];
