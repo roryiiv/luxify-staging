56e3a90b6415ade257626ab110ba600271ce07a7
Martins <max.lui@gmail.com>
2016-08-29 17:25:06 +0800

    Schema fix: price

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -157,7 +157,9 @@
                                 <li><span class="icon icon-tag"></span><span class="text">{{ $price_format }}</span></li>
                                 <li><span class="icon icon-globe"></span><span class="text">{{ isset($country) ? $country->name : '' }}</span></li>
                                @if($listing->price)
-                                  <div style="display:none;" itemprop="price" {{schema::itemType('Integer')}}>{{$listing->price}}</div>
+                                <div style="display:none;" {{schema::itemType('Offer')}}>
+                                  <div style="display:none;" {{schema::itemProp('price')}} {{schema::itemType('Integer')}}>{{$listing->price}}</div>
+                                </div>
                                @endif
                             </ul>
                             <ul class="social-links">
