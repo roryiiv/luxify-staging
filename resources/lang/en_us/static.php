e86ddbb336ffc22e59f2d751b479b09dffabaece
Arung Isyadi <arungisyadi@outlook.com>
2016-09-01 00:02:28 +0700

    Additional files for static content pages.

---
+++ resources/lang/en_us/static.php
@@ -0,0 +1,176 @@
+<?php
+
+//en_us - frontend - footer
+//for file: index.blade.php
+return[
+'index_banner_up'=>'Luxury Within Reach',
+'index_banner_down'=>'Asia’s leading marketplace for luxury',
+
+'index_banner_realestates'=>'Real Estates',
+'index_banner_watches&jewelry'=>'Watches & Jewelry',
+'index_banner_motors'=>'Motors',
+'index_banner_handbags&accessories'=>'Handbags & Accessories',
+'index_banner_experiences'=>'Experiences',
+'index_banner_collectibles&furnitures'=>'Collectibles & Furnitures',
+'index_banner_yachts'=>'Yachts',
+'index_banner_aircrafts'=>'Aircrafts',
+'index_banner_art&antiques'=>'Art & Antiques',
+'index_banner_finewines&spirits'=>'Fine Wines & Spirits',
+
+
+
+'index_banner_total_market'=>'Total Market in USD',
+'index_banner_total_listing'=>'Total Listings',
+'index_banner_total_newlisting'=>'New Listings In The Past 30 Days',
+
+
+'index_feature_title'=>'FEATURED',
+'index_feature_label'=>'“Rees Rex”',
+
+'index_feature_description'=>'One of the most complete and well-preserved skulls of any Tyrannosaurus rex ever discovered, is now available ',
+
+'index_feature_description_sale'=>'for sale.',
+
+'index_feature_view'=>'View',
+
+'index_desc1'=>'We Make Luxury Shopping Easier',
+'index_desc2'=>'Luxify is the perfect place to discover, search and browse through one of the Internet’s largest collections of new, vintage and pre-owned luxury goods in a safe and simple way.',
+
+'index_desc3'=>'We Are A Lead Generation Platform',
+
+'index_desc4'=>'We will never ask you for your payment details. Instead we match buyers with reputable sellers who have each been carefully handpicked and vetted by us. This ensures you are matched with genuine sellers and can enjoy complete peace of mind when browsing with us',
+
+'index_desc5'=>'Compare Products From All Sellers',
+
+'index_desc6'=>'When you have found a product you are interested in, we provide an easy way for you to communicate with the seller. You can discuss transactions, arrange viewings, or discuss prices; all on a straightforward one to one basis',
+
+'index_desc7'=>'You Will Find Your Luxury Item',
+'index_desc8'=>'Whether you are looking for a unique timepiece, a luxury property or some fine wines for your cellar, you will find exactly what you are looking for on Luxify',
+
+'index_search1'=>'Search &amp; Compare',
+
+'index_search2'=>'Search and compare through one of the Internet’s largest collections of new, vintage and pre-owned luxury goods.',
+
+'index_search3'=>'View more',
+
+'index_discover1'=>'Discover',
+'index_discover2'=>'Why Luxify',
+
+'index_discover3'=>'We make online luxury shopping easier and more transparent.',
+
+'index_discover4'=>'View more',
+
+'index_3D_1'=>'3D Virtual Property Tour',
+'index_3D_2'=>'Enjoy a new luxury property viewing experience',
+'index_3D_3'=>'View more',
+
+'index_subscribe_1'=>'Subscribe to Luxify\'s ultimate luxury newsletter!',
+'index_subscribe_2'=>'Get the latest luxury finds in your inbox',
+'index_subscribe_3'=>'Subscribe',
+
+
+
+//for file: about.blade.php
+'about_aboutus'=>'About Us',
+
+'about_asialeading'=>'Asia’s Leading Online Marketplace for Luxury',
+
+'about_watchvideo'=>'Watch video',
+
+'about_whatluxify'=>'What\'s Luxify',
+
+'about_luxifyasia'=>'Luxify is Asia’s leading online marketplace for luxury. Our website is the go-to destination for luxury enthusiasts and collectors in Asia, providing easy, safe and reliable market access to the luxury market.',
+
+'about_onluxify'=>'On Luxify you will discover one of the Internet\'s largest collections of luxury products. Our website is the perfect place to discover, search and browse through a whole host of the very finest luxury goods.”',
+
+'about_itworks'=>'How it works?',
+
+'about_luxify_cofounders'=>'Luxify Co-Founders',
+
+'about_florian_desc'=>'Florian Martigny was born in Paris and started his career in New York with large European investment bank. After seven years in the banking industry, Martigny managed and sold his own cosmetic company in the US. Upon selling his company, he went on to enjoy a successful part two career in investment banking in Hong Kong where he has been based for the past nine years. Martigny is an avid kite surfer and yachting enthusiast.',
+
+'about_florian'=>'Florian Martigny',
+
+'about_cofounder_1'=>'Co-Founder ',
+
+'about_alexis_desc'=>'Alexis Zirah was born in Paris, educated in Australia. His career has taken him from Paris to Sydney, Seoul, Shanghai and Hong Kong where he is based since 2008. He is an ex-management consultant with a top international consulting company and has a successful background in developing internet companies in Asia. Alexis holds a private pilot license and often compete in ultramarathons around the world.',
+
+'about_alexis'=>'Alexis Zirah',
+
+'about_cofounder2'=>'Co-Founder',
+
+'about_joinus'=>'Join us',
+
+'about_looking'=>'Luxify is always looking for smart and passionate individuals to join our team in Hong Kong, Singapore or London. If you have an interest or experience in the luxury industry, email us at',
+
+'about_careers'=>'careers@luxify.com',
+
+'about_business'=>'Business Developer',
+
+'about_talented'=>'Are you a talented, dynamic sales professional with a proven track record of success? Join Luxify as a Business Developer and help take a high-growth company to the next level. You\'ll work in Hong Kong to both prospect and grow your own sales pipeline.',
+
+'about_analyst'=>'SEM Analyst',
+
+'about_dynamic'=>'Are you a dynamic, analytical SEM professional with a passion for challenges? Join Luxify as an SEM Associate! In this critical role, you\'ll help us increase that number exponentially by planning, designing, and building highly- scaled search campaigns across multiple engines.',
+
+'about_senior_designer'=>'Senior Designer',
+
+'about_marketing'=>'As a member of our Marketing team in Hong Kong, you will be responsible for creating both new design and executing against current brand standards in all channels, with a focus on web and social.',
+
+'about_apply'=>'Apply now',
+
+'about_create_account'=>'Create your account for free',
+
+'about_started'=>'Get Started',
+
+//for file: why-luxify.blade.php
+
+'why_luxify_shop'=>'Why Shop at Luxify',
+
+'why_luxify_discover'=>'On Luxify you will discover one of the Internet’s largest collections of luxury products and experiences',
+
+'why_luxify_facebook'=>'Sign up with Facebook',
+
+'why_luxify_twitter'=>'Sign up with Twitter',
+
+'why_luxify_register'=>'Register Now',
+
+'why_luxify_shopping'=>'We Make Luxury Shopping Easier',
+
+'why_luxify_perfect'=>'Luxify is the perfect place to discover, search and browse through a whole host of the very finest new, vintage and pre-owned luxury goods in a safe and simple way.',
+
+'why_luxify_lead'=>'We Are A Lead Generation Platform',
+
+'why_luxify_payment'=>'We will never ask you for your payment details. Instead we match buyers with reputable sellers who have each been carefully handpicked and vetted by us. This ensures you are matched with genuine sellers and can enjoy complete peace of mind when browsing with us',
+
+'why_luxify_compare'=>'Compare Products From All Sellers',
+
+'why_luxify_interest'=>'When you have found a product you are interested in, we provide an easy way for you to communicate with the seller. You can discuss transactions, arrange viewings, or discuss prices; all on a straightforward one to one basis.',
+
+'why_luxify_find'=>'You Will Find Your Luxury Item',
+
+'why_luxify_unique'=>'Whether you are looking for a unique timepiece, a luxury property or some fine wines for your cellar, you will find exactly what you are looking for a Luxify',
+
+'why_luxify_how'=>'How it works?',
+
+'why_luxify_simply'=>'Luxify simply connects reputable luxury dealers with luxury buyers in Asia',
+
+'why_luxify_reliable'=>'who need a reliable source of luxury goods around the world',
+
+'why_luxify_discover1'=>'Discover',
+
+'why_luxify_browse'=>'Browse and find your desired item accordingly',
+
+'why_luxify_compare1'=>'Compare',
+
+'why_luxify_brand'=>'Compare brand, price, and where the item is based',
+
+'why_luxify_seller'=>'Contact Seller Directly',
+
+'why_luxify_communicate'=>'Communicate directly with the seller',
+
+'why_luxify_create'=>'Create your account for free',
+
+'why_luxify_get_started'=>'Get Started'
+
+];
