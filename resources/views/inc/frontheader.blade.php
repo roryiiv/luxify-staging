5d5354852415cbff1a6780d99997a677a3b641ec
martins.lui@luxify.com <ubuntu@ip-172-31-30-150.ap-southeast-1.compute.internal>
2016-09-07 01:02:16 +0000

    Replace the latest header with the outdated one

--- resources/views/inc/frontheader.blade.php
+++ resources/views/inc/frontheader.blade.php
@@ -5,7 +5,7 @@
                 <!-- menu opener and logo -->
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">Menu<span></span></button>
                 <!-- logo of the page -->
-                <a class="navbar-brand" href="{{func::set_url('/')}}">
+                <a class="navbar-brand" href="/">
                     <img src="{{func::img_url('luxify-logo.png', '', '', false, true)}}" alt="Luxify" class="normal">
                 </a>
             </div>
@@ -15,27 +15,34 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">@lang('header.shop')</a>
                         <div class="dropdown-menu">
-                        <?php $categories = DB::table('category_2')->where('parent',0)->get(); ?>
-
                             <ul>
-                                <li><a href="{{func::set_url('/luxify-estates/3d-estates')}}">@lang('header.shop_luxifyEstate3d')</a></li>
-                                @foreach($categories as $value)
-                                    <li><a href="{{func::set_url('/category/'.$value->slug)}}">@lang('categories.'.$value->slug)</a></li>    
-                                @endforeach
+                                <li><a href="/luxify-estates/3d-estates">@lang('header.shop_luxifyEstate3d')</a></li>
+                                <li><a href="/category/real-estates">@lang('header.shop_realEstates')</a></li>
+                                <li><a href="/category/jewellery-watches">@lang('header.shop_whatches&Jewelry')</a></li>
+                                <li><a href="/category/motors">@lang('header.shop_motors')</a></li>
+                                <li><a href="/category/handbags-accessories">@lang('header.shop_handbags&Accessories')</a></li>
+                                <li><a href="/category/experiences">@lang('header.shop_experiences')</a></li>
+                                <li><a href="/category/collectibles-furnitures">@lang('header.shop_collectibles&Furnitures')</a></li>
+                                <li><a href="/category/yachts">@lang('header.shop_yachts')</a></li>
+                                <li><a href="/category/aircrafts">@lang('header.shop_aircrafts')</a></li>
+                                <li><a href="/category/art-antiques">@lang('header.shop_art&Antiques')</a></li>
+                                <li><a href="/category/fine-wines-spirits">@lang('header.shop_fineWines&Spirits')</a></li>
                             </ul>
                         </div>
                     </li>
-                    <li><a href="{{func::set_url('/why-luxify')}}">@lang('header.whyLuxify')</a></li>
-                    <li><a target="_blank" href="{{func::set_url('/blog')}}">@lang('header.blog')</a></li>
+                    <li><a href="/why-luxify">@lang('header.whyLuxify')</a></li>
+                    <li><a target="_blank" href="/blog">@lang('header.blog')</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">@lang('header.more')</a>
                         <div class="dropdown-menu sm">
                             <ul>
-                                <li><a href="{{func::set_url('/about')}}">@lang('header.more_aboutLuxify')</a></li>
-                                <li><a href="{{func::set_url('/pricing')}}">@lang('header.more_pricing')</a></li>
-                                <li><a href="{{func::set_url('/dealer-application')}}">@lang('header.more_dealerApplication')</a></li>
-                                <li><a href="{{func::set_url('/luxify-estates')}}">@lang('header.more_luxifyEstates')</a></li>
-                                <li><a href="{{func::set_url('/contact')}}">@lang('header.more_contactUs')</a></li>
+                                <li><a href="/about">@lang('header.more_aboutLuxify')</a></li>
+                                <li><a href="/pricing">@lang('header.more_pricing')</a></li>
+                                <li><a href="/press">@lang('footer.press')</a></li>
+                                <li><a href="/dealer-application">@lang('header.more_dealerApplication')</a></li>
+                                <li><a href="/luxify-estates">@lang('header.more_luxifyEstates')</a></li>
+                                <li><a href="/contact">@lang('header.more_contactUs')</a></li>
+                                <li><a href="/faq">@lang('header.more_faq')</a></li>
                             </ul>
                         </div>
                     </li>
@@ -66,24 +73,24 @@
                           <div class="dropdown-menu" id="user-menu">
                               <ul>
                                   @if(Auth::user()->role == 'admin')
-                                    <li><a href="{{func::set_url('/panel')}}">@lang('header.adminPanel')</a></li>
+                                    <li><a href="/panel">@lang('header.adminPanel')</a></li>
                                   @else
-                                    <li><a href="{{func::set_url('/dashboard')}}">@lang('header.dashboard')</a></li>
+                                    <li><a href="/dashboard">@lang('header.dashboard')</a></li>
                                   @endif
-                                  <li><a href="{{func::set_url('/logout')}}">@lang('header.logout')</a></li>
+                                  <li><a href="/logout">@lang('header.logout')</a></li>
                               </ul>
                           </div>
                         </li>
                     </ul>
                 @else
                     <ul class="nav navbar-nav navbar-right">
-<!--                         <li class="currency-selector-container">
+                        <li class="currency-selector-container">
                             <select id="langSelect" class="language-selector">
                                 @foreach($languages as $language)
                                     <option value="{{$language['val']}}"{{func::selected($language['code'], $sess_lang)}}>{{$language['label']}}</option>
                                 @endforeach
                             </select>
-                        </li> -->
+                        </li>
                         <li class="currency-selector-container">
                            <select id="currSelect" class="currency-selector">
                              @foreach($currencies as $currency)
@@ -91,8 +98,8 @@
                              @endforeach
                            </select>
                         </li>
-                        <li><a href="{{func::set_url('/register')}}">@lang('header.signUp')</a></li>
-                        <li><a href="{{func::set_url('/login')}}">@lang('header.login')</a></li>
+                        <li><a href="/register">@lang('header.signUp')</a></li>
+                        <li><a href="/login">@lang('header.login')</a></li>
                     </ul>
                 @endif
             </div>
